a35fc7e5f34277cc1419a6b6b1989ea4b0afad8e
Inessa-kch <inessa.kechek@etu.sorbonne-universite.fr>
2024-11-19 09:39:18 +0100

    added gradient clipping, saving/loading generations

--- src/DQNetwork/model.py
+++ src/DQNetwork/model.py
@@ -1,9 +1,16 @@
 import torch as T
+import os
 
-def save_model(agent, filename):
-    """Saves the model's state dictionary to a specified file."""
-    T.save(agent.Q_eval.state_dict(), filename)
-    print(f"Model saved to {filename}")
+def save_model(agent,generation):
+    model_path = f'checkpoints/model_gen_{generation}.pth'
+    T.save(agent.Q_eval.state_dict(), model_path)
+    print(f"Model saved as generation {generation}")
+
+
+def get_latest_generation():
+    files = os.listdir('checkpoints')
+    generations = [int(f.split('_')[2].split('.')[0]) for f in files if f.startswith('model_gen_')]
+    return max(generations) if generations else 0
 
 def load_model(agent, filename):
     """Loads the model's state dictionary from a specified file."""
@@ -12,3 +19,10 @@ def load_model(agent, filename):
     print(f"Model loaded from {filename}")
 
 
+def load_latest_model(agent):
+    generation = get_latest_generation()
+    if generation > 0:
+        model_path = f'checkpoints/model_gen_{generation}.pth'
+        load_model(agent, model_path)
+        print(f"Model generation {generation} loaded successfully")
+    return generation
